7ed5d5a3e644c0bd1060b9ae1b80423b25f8a062
Spencer Haan <spencer@haan.dev>
2025-12-03 14:58:19 -0500

    Day 2 - Part 2

--- day_3/main.py
+++ day_3/main.py
@@ -1,25 +1,41 @@
 import puzzles
 
+def find_largest_battery(battery_bank):
+    index = 0
+    largest_battery = 0
 
-def find_largest_joltage(battery_bank):
-    largest_battery_start = 0
-    largest_battery_start_index = 0
-    for i, battery in enumerate(battery_bank[:-1]):
-        if largest_battery_start < int(battery):
-            largest_battery_start = int(battery)
-            largest_battery_start_index = i
+    for i, b in enumerate(battery_bank):
+        battery = int(b)
+        if battery > largest_battery:
+            index = i
+            largest_battery = battery
 
-    largest_battery_end = 0
-    for battery in battery_bank[largest_battery_start_index+1:]:
-        if largest_battery_end < int(battery):
-            largest_battery_end = int(battery)
+    return index, largest_battery
 
-    return largest_battery_start * 10 + largest_battery_end
+def find_largest_joltage(battery_bank, max_battery_count = 2):
+    selected_batteries = []
+    battery_bank_length = len(battery_bank)
+
+    end_index = battery_bank_length
+    while max_battery_count > 0:
+        index, largest = find_largest_battery(battery_bank[:end_index])
+
+        if index + max_battery_count > len(battery_bank):
+            end_index = index
+            continue
+
+        selected_batteries.append(largest)
+        max_battery_count -= 1
+        battery_bank = battery_bank[index + 1:]
+
+        end_index = battery_bank_length
+
+    return int("".join(map(str, selected_batteries)))
 
 def run():
     total_joltage_output = 0
     for battery_bank in puzzles.lines("day_3"):
-        total_joltage_output += find_largest_joltage(battery_bank)
+        total_joltage_output += find_largest_joltage(battery_bank, 12)
 
     print("The total joltage output is", total_joltage_output)
 
--- tests/test_day_3.py
+++ tests/test_day_3.py
@@ -4,17 +4,23 @@ import day_3
 
 
 class TestDay3(unittest.TestCase):
-    def test_something(self):
+    def test_find_largest_joltage(self):
         test_cases = [
-            ("987654321111111", 98),
-            ("811111111111119", 89),
-            ("234234234234278", 78),
-            ("818181911112111", 92),
-            ("2232212212212222211221231124224222213132222133122224222123222112324122222122221322222225222342243112", 54),
+            ("987654321111111", 2, 98),
+            ("811111111111119", 2, 89),
+            ("234234234234278", 2, 78),
+            ("818181911112111", 2, 92),
+            ("2232212212212222211221231124224222213132222133122224222123222112324122222122221322222225222342243112", 2, 54),
+            ("987654321111111", 3, 987),
+            ("811111111111119", 3, 819),
+            ("987654321111111", 12, 987654321111),
+            ("811111111111119", 12, 811111111119),
+            ("234234234234278", 12, 434234234278),
+            ("818181911112111", 12, 888911112111),
         ]
-        for battery_bank, expected_joltage in test_cases:
-            with self.subTest(test_case=battery_bank, expected_joltage=expected_joltage):
-                actual_joltage = day_3.find_largest_joltage(battery_bank)
+        for battery_bank, max_battery_count, expected_joltage in test_cases:
+            with self.subTest(test_case=battery_bank, max_battery_count=max_battery_count, expected_joltage=expected_joltage):
+                actual_joltage = day_3.find_largest_joltage(battery_bank, max_battery_count)
 
                 self.assertEqual(expected_joltage, actual_joltage)
 
