2e664c0064ba9e7feadbe0516ac00bb2b75157c3
Spencer Haan <spencer@haan.dev>
2025-12-05 17:15:17 -0500

    Day 5 - Part 2

--- day_5/main.py
+++ day_5/main.py
@@ -1,15 +1,37 @@
 import puzzles
 
+def compress_ranges(ranges):
+    compressed_ranges = []
+    current_range = None
+    for r in sorted(ranges, key=lambda _r: _r.start):
+        if current_range is None:
+            current_range = r
+            continue
+        if r.start <= current_range.stop:
+            current_range = range(
+                current_range.start,
+                current_range.stop if current_range.stop > r.stop else r.stop
+            )
+        else:
+            compressed_ranges.append(current_range)
+            current_range = r
+    compressed_ranges.append(current_range)
+
+    return compressed_ranges
+
+
 class IngredientDb:
     def __init__(self, ingredients_ranges):
-        self.ingredient_ranges = ingredients_ranges
+        self.__ingredient_ranges = compress_ranges(ingredients_ranges)
 
     def __str__(self):
         string = ""
-        for ingredient_range in self.ingredient_ranges:
+        for ingredient_range in self.__ingredient_ranges:
             string += f"{ingredient_range.start}-{ingredient_range.stop}\n"
-
         return string
+
+    def ranges(self):
+        return self.__ingredient_ranges
 
     @staticmethod
     def parse(string):
@@ -20,14 +42,19 @@ class IngredientDb:
             start = int(start)
             end = int("".join(end)) + 1
             ingredient_ranges.append(range(start, end))
-
         return IngredientDb(ingredient_ranges)
 
     def is_fresh(self, ingredient):
-        for ingredient_range in self.ingredient_ranges:
+        for ingredient_range in self.__ingredient_ranges:
             if ingredient in ingredient_range:
                 return True
         return False
+
+    def considered_fresh(self):
+        total_fresh_ingredients = 0
+        for compressed_range in self.__ingredient_ranges:
+            total_fresh_ingredients += compressed_range.stop - compressed_range.start
+        return total_fresh_ingredients
 
 
 def run():
@@ -42,7 +69,7 @@ def run():
             fresh_ingredients.append(available_ingredient)
 
     print(f"There is {len(fresh_ingredients)} fresh ingredients")
-
+    print(f"There are {db.considered_fresh()} ingredients considered fresh")
 
 if __name__ == '__main__':
     run()
--- tests/test_day_5.py
+++ tests/test_day_5.py
@@ -2,15 +2,15 @@ import unittest
 
 import day_5
 
+TEST_RANGES = [
+    range(3, 6),
+    range(10, 15),
+    range(16, 21),
+    range(12, 19),
+]
 
 class TestDay5(unittest.TestCase):
     def test_is_fresh(self):
-        db = day_5.IngredientDb([
-            range(3, 6),
-            range(10, 15),
-            range(16, 21),
-            range(12, 19),
-        ])
         test_cases = [
             (1, False),
             (5, True),
@@ -19,12 +19,44 @@ class TestDay5(unittest.TestCase):
             (17, True),
             (32, False),
         ]
-
+        db = day_5.IngredientDb(TEST_RANGES)
         for available_ingredient, expected_is_fresh in test_cases:
-            with self.subTest(available_ingredient=available_ingredient, expected_is_fresh=expected_is_fresh):
+            with self.subTest(test_case=available_ingredient, expected_is_fresh=expected_is_fresh):
                 actual_is_fresh = db.is_fresh(available_ingredient)
 
                 self.assertEqual(expected_is_fresh, actual_is_fresh)
+
+    def test_considered_fresh(self):
+        expected_considered_fresh = 14
+
+        db = day_5.IngredientDb(TEST_RANGES)
+
+        actual_considered_fresh = db.considered_fresh()
+
+        self.assertEqual(expected_considered_fresh, actual_considered_fresh)
+
+    def test_range_edge_cases(self):
+        test_cases = [
+            # One range inside another
+            ([range(1, 10), range(3, 7)], 9),
+            # One range overlaps the other
+            ([range(1, 7), range(3, 10)], 9),
+            # One range has a single gap between another
+            ([range(1, 5), range(6, 10)], 8),
+            # One range starts where another stops
+            ([range(1, 6), range(5, 10)], 9),
+            # Both ranges start the same but end differently
+            ([range(1, 6), range(1, 10)], 9),
+            # Both ranges start differently but end the same
+            ([range(1, 10), range(6, 10)], 9),
+        ]
+        for test_ranges, expected in test_cases:
+            with self.subTest(test_case=test_ranges, expected=expected):
+                db = day_5.IngredientDb(test_ranges)
+
+                actual = db.considered_fresh()
+
+                self.assertEqual(expected, actual)
 
 
 if __name__ == '__main__':
